662421d9c7e2daddb3d70519fde834c2b72b248a
Sarju <sarju.hansaliya@gmail.com>
2020-01-12 23:39:47 +0530

    Fix bable runtime regenerator

--- .babelrc.js
+++ .babelrc.js
@@ -1,46 +1,47 @@
 let presets = [
-  '@babel/preset-env',
-  '@babel/preset-react',
-  '@babel/preset-flow',
-  '@lingui/babel-preset-react',
-  '@babel/preset-typescript',
+  "@babel/preset-env",
+  "@babel/preset-react",
+  "@babel/preset-flow",
+  "@lingui/babel-preset-react",
+  "@babel/preset-typescript",
   [
-    'minify',
+    "minify",
     {
       builtIns: false,
       evaluate: false,
-      mangle: false,
-    },
-  ],
+      mangle: false
+    }
+  ]
 ];
 let plugins = [
-  '@babel/plugin-proposal-class-properties',
-  '@babel/plugin-transform-async-to-generator',
-  '@babel/plugin-syntax-dynamic-import',
-  '@babel/plugin-syntax-import-meta',
-  '@babel/plugin-proposal-json-strings',
-  '@babel/plugin-proposal-object-rest-spread',
-  '@babel/plugin-transform-spread',
-  ['@babel/plugin-proposal-decorators', { legacy: true }],
-  'import-md-to-js',
+  "@babel/plugin-transform-runtime",
+  "@babel/plugin-proposal-class-properties",
+  "@babel/plugin-transform-async-to-generator",
+  "@babel/plugin-syntax-dynamic-import",
+  "@babel/plugin-syntax-import-meta",
+  "@babel/plugin-proposal-json-strings",
+  "@babel/plugin-proposal-object-rest-spread",
+  "@babel/plugin-transform-spread",
+  ["@babel/plugin-proposal-decorators", { legacy: true }],
+  "import-md-to-js",
   [
-    'module-resolver',
+    "module-resolver",
     {
-      root: ['.'],
+      root: ["."],
       alias: {
-        components: './src',
-        shared: '.',
-        images: './src/assets/images',
-      },
-    },
-  ],
+        components: "./src",
+        shared: ".",
+        images: "./src/assets/images"
+      }
+    }
+  ]
 ];
 
-if (process.env['ENV'] === 'test' || process.env['NODE_ENV'] === 'test') {
-  plugins.push('@babel/plugin-transform-modules-commonjs');
-  plugins.push('@babel/plugin-proposal-export-default-from');
-  plugins.push('@babel/plugin-transform-flow-strip-types');
-  plugins.push('dynamic-import-node');
+if (process.env["ENV"] === "test" || process.env["NODE_ENV"] === "test") {
+  plugins.push("@babel/plugin-transform-modules-commonjs");
+  plugins.push("@babel/plugin-proposal-export-default-from");
+  plugins.push("@babel/plugin-transform-flow-strip-types");
+  plugins.push("dynamic-import-node");
 }
 
 module.exports = { presets, plugins };
